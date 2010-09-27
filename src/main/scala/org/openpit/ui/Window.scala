7ac3c3cbc62f9c8685a7568fbbc47881ca3e216d
Ben Jackson <ben@ben.com>
2010-09-26 18:24:47 -0700

    Comment out sync(framerate) so big drops in performance are noticable

--- src/main/scala/org/openpit/ui/Window.scala
+++ src/main/scala/org/openpit/ui/Window.scala
@@ -29,7 +29,7 @@ object Window {
         import console.FPS
 
         if (Display.isVisible()) {
-            Display.sync(framerate)
+            //Display.sync(framerate)
             glClear(GL_COLOR_BUFFER_BIT |
                     GL_STENCIL_BUFFER_BIT |
                     GL_DEPTH_BUFFER_BIT)
