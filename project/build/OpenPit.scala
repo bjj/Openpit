eb5afd2c36029e2889490f6745c2e6806c0caded
Ben Jackson <ben@ben.com>
2010-09-27 16:18:20 -0700

    Add 'specs' dependency (download it with 'sbt update' -- it's not automatic) Add a sample "HelloWorld" test which you can run with 'sbt test'

--- project/build/OpenPit.scala
+++ project/build/OpenPit.scala
@@ -4,4 +4,8 @@ class OpenPitProject(info: ProjectInfo) extends LWJGLProject(info)
 {
     override def compileOptions = super.compileOptions ++
         Seq(Unchecked, Optimize)
+
+    val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.6-SNAPSHOT" % "test" withSources()
+    //val junit = "junit" % "junit" % "4.7" % "test"
+    val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
 }
---
+++ src/test/scala/HelloWorld.scala
@@ -0,0 +1,6 @@
+import org.specs._
+
+class helloWorld extends Specification {
+    "'hello world' has 11 characters" in {}
+    "'hello world' matches 'h.* w.*'" in {}
+}
