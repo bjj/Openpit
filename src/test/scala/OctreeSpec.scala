570ed493de7409f11afe4fbb4fa10af75dca937c
Ben Jackson <ben@ben.com>
2010-09-27 21:28:20 -0700

    Add Octree.foreach test

--- src/test/scala/OctreeSpec.scala
+++ src/test/scala/OctreeSpec.scala
@@ -1,5 +1,7 @@
 package org.openpit.test
 
+import collection.mutable.HashSet
+
 import org.openpit.util.Octree
 import simplex3d.math.intm._
 
@@ -18,5 +20,14 @@ object OctreeSpec extends Properties("Octree") {
     property("multiple storage") = {
         val o = new Octree[Int](Vec3i(0,0,0), 2)
         forAll { (c: Vec3i, p: Vec3i, v: Int) => o(p) = v; o(p) == v }
+    }
+
+    // XXX can't use Int here because Octree uses "null" for "nonexist" :(
+    property("foreach") = forAll { (c: Vec3i, s: Set[String] ) =>
+        val o = new Octree[String](c,2)
+        // Put the random set in random locations (locations ignored)
+        val where = Arbitrary.arbitrary[Vec3i]
+        for (i <- s) { o(where.sample.get) = i }
+        s == (HashSet() ++ o)
     }
 }
