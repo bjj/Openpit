481a827e8423dbab28d8484479d2d6320c037aad
Kenny Root <kenny@the-b.org>
2010-09-28 23:18:52 -0700

    Use a timer to determine camera speed
    Use LWJGL's timer to calculate how long it has been since the last
    update to the simulation.

--- src/main/scala/org/openpit/ui/Main.scala
+++ src/main/scala/org/openpit/ui/Main.scala
@@ -1,5 +1,6 @@
 package org.openpit.ui
 
+import org.lwjgl.Sys
 import org.lwjgl.opengl.Display
 import org.lwjgl.input.{Keyboard, Mouse}
 
@@ -7,7 +8,12 @@ import org.openpit.ui.console.FPS
 import org.openpit.world.World
 
 object Main {
+    val mouseSensitivity = 3f
+    val unitsPerSecond = 18.0
+    lazy val ticksPerSecond : Double = Sys.getTimerResolution()
+
     var finished = false
+    var lastTime = 0.0
 
     def main(args: Array[String]) {
 
@@ -30,8 +36,14 @@ object Main {
         Mouse.setGrabbed(true)
 
         FPS.start()
+
+        lastTime = getCurrentTime()
     }
 
+    def getCurrentTime() = {
+        Sys.getTime() / ticksPerSecond
+    }
+ 
     def cleanup() {
         Mouse.setGrabbed(false)
     }
@@ -45,15 +57,23 @@ object Main {
             println("exit clicked")
         }
 
+        var currentTime = getCurrentTime()
+        var elapsedTime = currentTime - lastTime
+        lastTime = currentTime
+
+        var movement = unitsPerSecond * elapsedTime
+        var movementFloat = movement.toFloat
+
         import org.openpit.ui.Camera
-        Camera.update(-Mouse.getDX * 0.3f, Mouse.getDY * 0.3f)
+        Camera.update(-Mouse.getDX * movementFloat * mouseSensitivity,
+                       Mouse.getDY * movementFloat * mouseSensitivity)
 
-        if (Keyboard.isKeyDown(Keyboard.KEY_A)) Camera.strafe(-0.3)
-        if (Keyboard.isKeyDown(Keyboard.KEY_S)) Camera.walk(-0.3)
-        if (Keyboard.isKeyDown(Keyboard.KEY_D)) Camera.strafe(0.3)
-        if (Keyboard.isKeyDown(Keyboard.KEY_W)) Camera.walk(0.3)
+        if (Keyboard.isKeyDown(Keyboard.KEY_A)) Camera.strafe(-movement)
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) Camera.walk(-movement)
+        if (Keyboard.isKeyDown(Keyboard.KEY_D)) Camera.strafe(movement)
+        if (Keyboard.isKeyDown(Keyboard.KEY_W)) Camera.walk(movement)
 
-        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) Camera.update(10, 0)
-        if (Keyboard.isKeyDown(Keyboard.KEY_E)) Camera.update(-10, 0)
+        if (Keyboard.isKeyDown(Keyboard.KEY_Q)) Camera.update(movementFloat * mouseSensitivity, 0)
+        if (Keyboard.isKeyDown(Keyboard.KEY_E)) Camera.update(-movementFloat * mouseSensitivity, 0)
     }
 }
