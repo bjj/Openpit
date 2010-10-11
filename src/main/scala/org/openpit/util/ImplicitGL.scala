83b130ea7d6dc978235393b31800bd69839c5f25
Ben Jackson <ben@ben.com>
2010-10-10 22:48:33 -0700

    Add implicit Array[Int] to IntBuffer conversion for quickie OpenGL calls

--- src/main/scala/org/openpit/util/ImplicitGL.scala
+++ src/main/scala/org/openpit/util/ImplicitGL.scala
@@ -1,7 +1,7 @@
 
 package org.openpit.util
 
-import java.nio.{ByteBuffer, FloatBuffer, ByteOrder}
+import java.nio.{ByteBuffer, IntBuffer, FloatBuffer, ByteOrder}
 
 
 object ImplicitGL {
@@ -9,6 +9,14 @@ object ImplicitGL {
     implicit def FloatArrayToFloatBuffer(a: Array[Float]): FloatBuffer = {
         val fb = ByteBuffer.allocateDirect(a.length * 4).
             order(ByteOrder.nativeOrder()).asFloatBuffer()
+        fb.put(a)
+        fb.rewind()
+        fb
+    }
+
+    implicit def IntArrayToIntBuffer(a: Array[Int]): IntBuffer = {
+        val fb = ByteBuffer.allocateDirect(a.length * 4).
+            order(ByteOrder.nativeOrder()).asIntBuffer()
         fb.put(a)
         fb.rewind()
         fb
